a049d852366a9f572cda13a7dc39f942271aac82
puchcharapoom-mangmool <6206021610111@fitm.kmutnb.ac.th>
2021-02-03 19:09:26 +0700

    save package

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-//require('dotenv').config();
+require("dotenv").config();
 
 const express = reqiure("express");
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 
-app.get("/",(req,res)=>{
-    res.send("Hello World!");
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
-app.listen(port,()=>{
-    console.log(`Example app listening at http://localhost:${port}`);
+app.listen(port, () => {console.log(`Example app listening at http://localhost:${port}`);
 });
